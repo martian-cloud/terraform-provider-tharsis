dd4017090bf888fb846373a913911d5a654e6a30
Robert Riches <robert.richesjr@infor.com>
2023-02-28 13:00:13 -0800

    Make acceptance tests passing again for VCS provider OAuthClient* write-only attributes.

--- internal/provider/resource_vcs_provider_test.go
+++ internal/provider/resource_vcs_provider_test.go
@@ -43,14 +43,19 @@ func TestVCSProvider(t *testing.T) {
 					resource.TestCheckResourceAttrSet("tharsis_vcs_provider.tvp", "last_updated"),
 					resource.TestCheckResourceAttrSet("tharsis_vcs_provider.tvp", "created_by"),
 					resource.TestCheckResourceAttrSet("tharsis_vcs_provider.tvp", "resource_path"),
+
+					// OAuthClientID and OAuthClientSecret are write-only, so there's nothing to verify here.
 				),
 			},
 
 			// Import the state.
+			// The OAuthClientID and OAuthClientSecret fields are write-only,
+			// so they cannot be verified during import.
 			{
-				ResourceName:      "tharsis_vcs_provider.tvp",
-				ImportState:       true,
-				ImportStateVerify: true,
+				ResourceName:            "tharsis_vcs_provider.tvp",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"oauth_client_id", "oauth_client_secret"},
 			},
 
 			// Update and read.
@@ -72,6 +77,8 @@ func TestVCSProvider(t *testing.T) {
 					resource.TestCheckResourceAttrSet("tharsis_vcs_provider.tvp", "last_updated"),
 					resource.TestCheckResourceAttrSet("tharsis_vcs_provider.tvp", "created_by"),
 					resource.TestCheckResourceAttrSet("tharsis_vcs_provider.tvp", "resource_path"),
+
+					// OAuthClientID and OAuthClientSecret are write-only, so there's nothing to verify here.
 				),
 			},
 
@@ -87,6 +94,8 @@ func testVCSProviderConfigurationCreate() string {
 	createHostname := "test-vcs-provider-hostname"
 	createType := "gitlab"
 	createAutoCreateWebhooks := true
+	createOAuthClientID := "tvp-oauth-client-id"
+	createOAuthClientSecret := "tvp-oauth-client-secret"
 
 	return fmt.Sprintf(`
 
@@ -99,9 +108,12 @@ resource "tharsis_vcs_provider" "tvp" {
 	group_path = tharsis_group.root-group.full_path
 	type = "%s"
 	auto_create_webhooks = %s
+	oauth_client_id = "%s"
+	oauth_client_secret = "%s"
 }
 	`, createRootGroup(), createName, createDescription,
-		createHostname, createType, strconv.FormatBool(createAutoCreateWebhooks))
+		createHostname, createType, strconv.FormatBool(createAutoCreateWebhooks),
+		createOAuthClientID, createOAuthClientSecret)
 }
 
 func testVCSProviderConfigurationUpdate() string {
@@ -111,6 +123,8 @@ func testVCSProviderConfigurationUpdate() string {
 	createAutoCreateWebhooks := true
 
 	updateDescription := "this is tvp's updated description"
+	updateOAuthClientID := "tvp-oauth-client-updated-id"
+	updateOAuthClientSecret := "tvp-oauth-client-updated-secret"
 
 	return fmt.Sprintf(`
 
@@ -123,9 +137,12 @@ resource "tharsis_vcs_provider" "tvp" {
 	group_path = tharsis_group.root-group.full_path
 	type = "%s"
 	auto_create_webhooks = %s
+	oauth_client_id = "%s"
+	oauth_client_secret = "%s"
 }
 	`, createRootGroup(), createName, updateDescription,
-		createHostname, createType, strconv.FormatBool(createAutoCreateWebhooks))
+		createHostname, createType, strconv.FormatBool(createAutoCreateWebhooks),
+		updateOAuthClientID, updateOAuthClientSecret)
 }
 
 // The End.
