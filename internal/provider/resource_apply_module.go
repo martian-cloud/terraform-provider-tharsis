169f61c03186dbf73fcf3bbb1acbccb0a7bb1853
Robert Riches <robert.richesjr@infor.com>
2023-04-19 15:22:18 -0700

    Troubleshooting: Escalate job status print to error to try to see it.

--- internal/provider/resource_apply_module.go
+++ internal/provider/resource_apply_module.go
@@ -481,7 +481,7 @@ func (t *applyModuleResource) waitForJobCompletion(ctx context.Context, jobID *s
 			}
 
 			// FIXME: Remove this:
-			tflog.Warn(ctx, fmt.Sprintf("***** in waitForJobCompletion: job status: %s", job.Status))
+			tflog.Error(ctx, fmt.Sprintf("***** in waitForJobCompletion: job status: %s", job.Status))
 
 			if job.Status == "finished" {
 				return nil
